dc90d14f5286cecdfc7e1c322d0d9c7ef5a0796b
Matheus gomes <126532702+MatheusGomes00@users.noreply.github.com>
2025-03-21 18:42:20 -0300

    adcionado metodos de busca por cpf e nome

--- ProjetoNPJ/src/main/java/com/npj/ProjetoNPJ/agenda/repository/AgendaRepository.java
+++ ProjetoNPJ/src/main/java/com/npj/ProjetoNPJ/agenda/repository/AgendaRepository.java
@@ -1,15 +1,20 @@
 package com.npj.ProjetoNPJ.agenda.repository;
 
+import com.npj.ProjetoNPJ.advogados.entity.Advogado;
 import com.npj.ProjetoNPJ.agenda.entity.Agendamento;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AgendaRepository extends MongoRepository<Agendamento, String> {
 
-    @Query("{ 'cpf': ?0 }")
-    Optional<Agendamento> findByCpf(String cpf);
+    @Query("{ 'nome': { $regex: ?0, $options: 'i' } }")
+    List<Agendamento> findByNome(String nome);
+
+    @Query("{ 'cpf': { $regex: ?0 } }")
+    List<Agendamento> findByCpf(String cpf);
 }
