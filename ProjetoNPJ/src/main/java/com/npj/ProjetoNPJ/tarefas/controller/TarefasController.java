7b90148fd714dc068d6992b50e0c5639dbdef104
Matheus gomes <126532702+MatheusGomes00@users.noreply.github.com>
2025-03-20 23:25:04 -0300

    removido comentarios

--- ProjetoNPJ/src/main/java/com/npj/ProjetoNPJ/tarefas/controller/TarefasController.java
+++ ProjetoNPJ/src/main/java/com/npj/ProjetoNPJ/tarefas/controller/TarefasController.java
@@ -1,22 +1,14 @@
 package com.npj.ProjetoNPJ.tarefas.controller;
 
 
-import com.npj.ProjetoNPJ.advogados.dtos.ResponseAdvogadoDto;
-import com.npj.ProjetoNPJ.advogados.entity.Advogado;
-import com.npj.ProjetoNPJ.advogados.repository.AdvogadoRepository;
-import com.npj.ProjetoNPJ.exceptions.RecursoNaoEncontradoException;
+
 import com.npj.ProjetoNPJ.tarefas.dtos.DtoTarefas;
-import com.npj.ProjetoNPJ.tarefas.entity.Tarefas;
-import com.npj.ProjetoNPJ.tarefas.mapper.TarefasMapper;
-import com.npj.ProjetoNPJ.tarefas.repository.TarefasRepository;
 import com.npj.ProjetoNPJ.tarefas.service.TarefefasService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Provider;
 import java.util.List;
 
 @RestController
@@ -24,13 +16,8 @@ import java.util.List;
 public class TarefasController {
 
     @Autowired
-    private AdvogadoRepository repositoryAdv;
-
-    @Autowired
     private TarefefasService service;
 
-    @Autowired
-    private TarefasRepository repository;
 
     @PostMapping(value = "/create")
     public ResponseEntity<DtoTarefas> criarTarefa(@RequestBody @Valid DtoTarefas tarefaDto) {
@@ -38,11 +25,6 @@ public class TarefasController {
         return ResponseEntity.ok().body(tarefa);
     }
 
-    //@GetMapping("/{tarefaId}/responsavel")
-    //public ResponseEntity<String> getNomeAdvogadoPorTarefa(@PathVariable String tarefaId) {
-      //  String nomeAdvogado = service.getNomeAdvogadoPorTarefa(tarefaId);
-       // return ResponseEntity.ok(nomeAdvogado);
-   // }
 
     @PutMapping(value = "/upd/{id}")
     public ResponseEntity<Void> update(@PathVariable String id, @RequestBody DtoTarefas dto) {
