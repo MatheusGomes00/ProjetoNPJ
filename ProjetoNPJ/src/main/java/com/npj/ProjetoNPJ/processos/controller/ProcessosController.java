92c66aa09833db2d6e702352bb2180fa0fcd210e
Erick Candido <erickoliveira1144@hotmail.com>
2025-04-03 21:32:22 -0300

    Tratando excessoes dos processos

--- ProjetoNPJ/src/main/java/com/npj/ProjetoNPJ/processos/controller/ProcessosController.java
+++ ProjetoNPJ/src/main/java/com/npj/ProjetoNPJ/processos/controller/ProcessosController.java
@@ -6,6 +6,7 @@ import com.npj.ProjetoNPJ.processos.repository.ProcessosRepositorio;
 import com.npj.ProjetoNPJ.processos.service.ProcessosService;
 import com.npj.ProjetoNPJ.tarefas.dtos.DtoTarefas;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,9 +25,17 @@ public class ProcessosController {
     @PostMapping(value = "/newProc")
     public ResponseEntity<DtoProcessos> insert(@RequestBody DtoProcessos dto, @RequestHeader("Authorization") String authorizationHeader){
 
-       DtoProcessos novoProcesso = service.insert(dto);
+        try {
+            DtoProcessos novoProcesso = service.insert(dto);
 
-       return ResponseEntity.ok().body(novoProcesso);
+            return ResponseEntity.ok().body(novoProcesso);
+        } catch (Exception e) {
+            System.err.println("Erro ao criar o processo!: " + e.getMessage());
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(null);
+        }
+
 
     }
     @PutMapping(value = "/updProc/{id}")
@@ -46,23 +55,46 @@ public class ProcessosController {
     @GetMapping(value = "/findAll")
     public ResponseEntity<List<DtoProcessos>> findAll(){
 
-        List<DtoProcessos> novoProcesso = service.findAll();
+        try {
+            List<DtoProcessos> novoProcesso = service.findAll();
 
-        return ResponseEntity.ok(novoProcesso);
+            return ResponseEntity.ok(novoProcesso);
+        } catch (Exception e) {
+            System.err.println("Erro ao procurar processos: " + e.getMessage());
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(null);
+        }
+
     }
 
     @PutMapping(value = "/finalizar/{id}")
     public ResponseEntity<Processos> excluirProc(@PathVariable String id){
+        try{
 
-        Processos processos = service.excluir(id);
-        return ResponseEntity.ok().body(processos);
+            Processos processos = service.excluir(id);
+            return ResponseEntity.ok().body(processos);
+        } catch (Exception e) {
+            System.err.println("Erro ao finalizar Processo: " + e.getMessage());
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(null);
+        }
+
     }
 
     @GetMapping(value = "/porId/{id}")
     public ResponseEntity<Processos> acharPorID(@PathVariable String id){
+        try {
+            Processos processos = service.findById(id);
+            return ResponseEntity.ok().body(processos);
+        } catch (Exception e) {
+            System.err.println("Erro ao procurar processo: " + e.getMessage());
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(null);
+        }
 
-        Processos processos = service.findById(id);
-        return ResponseEntity.ok().body(processos);
     }
 
 }
