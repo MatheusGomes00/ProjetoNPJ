924803671e6032d59b64ae486cdccc2a579bec76
Matheus gomes <126532702+MatheusGomes00@users.noreply.github.com>
2025-04-14 22:40:47 -0300

    ajustes insersão agendamentos

--- ProjetoNPJ/src/main/java/com/npj/ProjetoNPJ/agendamentos/entity/Agendamento.java
+++ ProjetoNPJ/src/main/java/com/npj/ProjetoNPJ/agendamentos/entity/Agendamento.java
@@ -4,6 +4,7 @@ import com.npj.ProjetoNPJ.agenda.dto.ResponsavelDto;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -23,8 +24,10 @@ public class Agendamento {
 
     private String cpf;
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime start;
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime end;
 
     private String casoTipo;
