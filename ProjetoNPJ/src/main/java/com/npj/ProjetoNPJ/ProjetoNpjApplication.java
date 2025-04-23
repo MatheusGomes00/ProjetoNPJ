513635d976ce381dab2ea52e1efa3aac57b2624b
Matheus gomes <126532702+MatheusGomes00@users.noreply.github.com>
2025-04-23 16:01:49 -0300

    ajuste de hora

--- ProjetoNPJ/src/main/java/com/npj/ProjetoNPJ/ProjetoNpjApplication.java
+++ ProjetoNPJ/src/main/java/com/npj/ProjetoNPJ/ProjetoNpjApplication.java
@@ -5,12 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
+import java.util.TimeZone;
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-
-
 public class ProjetoNpjApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(ProjetoNpjApplication.class, args);
 	}
 
