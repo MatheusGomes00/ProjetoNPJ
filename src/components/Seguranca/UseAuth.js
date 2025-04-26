bacaa8e5cee43b304a018d934dbe2dc338bd40de
Matheus gomes <126532702+MatheusGomes00@users.noreply.github.com>
2025-04-25 21:59:06 -0300

    alterada lógica para recuperar ID no UseAuth

--- src/components/Seguranca/UseAuth.js
+++ src/components/Seguranca/UseAuth.js
@@ -24,7 +24,7 @@ const useAuth = () => {
       const token = getAccessToken();
       if (!token) return null;
       const decoded = jwtDecode(token);
-      return decoded.id;
+      return decoded.sub;
     }
 
     const getRole = () => {
