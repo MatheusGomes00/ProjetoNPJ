eddb0a60868df81b71e0e6617e8e197f34a62ca8
Matheus gomes <126532702+MatheusGomes00@users.noreply.github.com>
2025-05-07 22:32:21 -0300

    teste processo

--- ProjetoNPJ/src/main/java/com/npj/ProjetoNPJ/processos/controller/ProcessosController.java
+++ ProjetoNPJ/src/main/java/com/npj/ProjetoNPJ/processos/controller/ProcessosController.java
@@ -52,20 +52,20 @@ public class ProcessosController {
         return ResponseEntity.ok(processos);
     }
 
-//    @GetMapping(value = "/findAll")
-//    public ResponseEntity<List<DtoProcessos>> findAll(){
-//
-//        try {
-//            List<DtoProcessos> novoProcesso = service.findAll();
-//
-//            return ResponseEntity.ok(novoProcesso);
-//        } catch (Exception e) {
-//            System.err.println("Erro ao procurar processos: " + e.getMessage());
-//            e.printStackTrace();
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-//                    .body(null);
-//        }
-//    }
+    @GetMapping(value = "/findAll")
+    public ResponseEntity<List<DtoProcessos>> findAll(){
+
+        try {
+            List<DtoProcessos> novoProcesso = service.findAll();
+
+            return ResponseEntity.ok(novoProcesso);
+        } catch (Exception e) {
+            System.err.println("Erro ao procurar processos: " + e.getMessage());
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(null);
+        }
+    }
 
     @PutMapping(value = "/finalizar/{id}")
     public ResponseEntity<Processos> excluirProc(@PathVariable String id){
